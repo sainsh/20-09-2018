b4fb334a251aa43213610de1904c56e25152a499
sainsh <kfsainsh@gmail.com>
2018-09-20 09:57:41 +0200

    second

--- src/Dice.java
+++ src/Dice.java
@@ -1,5 +1,12 @@
+import java.util.Random;
+
 public class Dice {
 
-    
+    Random rand = new Random();
+
+    public int rollDice(){
+
+       return rand.nextInt(6)+1;
+    }
 
 }
--- src/Logic.java
+++ src/Logic.java
@@ -1,9 +1,42 @@
+import java.util.Scanner;
+
 public class Logic {
 
     Dice dice = new Dice();
+    int player1 = 0;
+    int player2 = 0;
+    Scanner scan = new Scanner(System.in);
+
+
 
     public void run(){
 
+        System.out.println("game begins");
+
+        player1=0;
+        player2=0;
+
+        while(true){
+
+            System.out.println("rolling dice");
+            player1=dice.rollDice();
+            System.out.println(player1);
+            player2=dice.rollDice();
+            System.out.println(player2);
+
+            if(player1 > player2){
+                System.out.println("player 1 wins");
+            }
+            else if(player2>player1){
+                System.out.println("player 2 wins");
+            }
+            else{
+                System.out.println("game is tied");
+            }
+            System.out.println("continue press any key + enter");
+
+            scan.next();
+        }
 
 
     }
